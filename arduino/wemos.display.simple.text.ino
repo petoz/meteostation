d85385d2075912747209350dd08b19a50484f048
petoz <36447879+petoz@users.noreply.github.com>
2018-02-20 21:40:25 +0100

    Update wemos.display.simple.text.ino

--- arduino/wemos.display.simple.text.ino
+++ arduino/wemos.display.simple.text.ino
@@ -2,8 +2,8 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 
-const char* ssid     = "optical";
-const char* password = "Passw0rd";
+const char* ssid     = "panoptical";
+const char* password = "password";
 
 #include <Adafruit_SSD1306.h>
 
