b9599d712bb71f9bf5be77be60ed2dee65f7dce0
petoz <36447879+petoz@users.noreply.github.com>
2018-05-10 09:06:49 +0200

    split every sensor update to different update

--- raspberry/sensor.api.py
+++ raspberry/sensor.api.py
@@ -15,7 +15,7 @@ api_pass = api_pass.rstrip()
 print api_pass
 
 #infile = '/home/pi/outtemp.txt'
-infile = '/var/log/mem/sensrf.txt'
+infile = '/var/log/mem/sensor.RF.Ext1.txt'
 infile = open(infile)
 line1 = infile.readline()
 line1 = line1.rstrip()
@@ -184,14 +184,16 @@ pressure = pressure * math.exp(gmhrtk)
 
 casi = int(time.time())
 cass = str(casi)
-api_string = "http://coffee.onmypc.net/meteotemplate/api.php?U=" + cass
+api_string = "http://coffee.onmypc.net/meteotemplate/api.php?"
 #full = api_string + line1 + "&TIN=" + str("%.2f" %cTemp) + "&P=" + str("%.2f" %pressure) + "&HIN=" + str("%.2f" %hum) + "&PASS=" + api_pass
 #full = api_string + line1 + "&P=" + str("%.2f" %pressure) + "&TIN=" + str("%.2f" %cTemp) + "&HIN=" + str("%.2f" %hum) + "&T1=" + str("%.2f" %temp) + "&PASS=" + api_pass
 #full = api_string + line1 + "&P=" + str("%.2f" %pressure) + "&TIN=" + str("%.2f" %cTemp) + "&HIN=" + str("%.2f" %hum) + "&T1=" + str("%.2f" %temp) + "&T2=" + str("%.2f" %read_temp()) + "&PASS=" + api_pass
 # 9.6.2017 add parameter W,G,B,R,RR,S to disable PHP Notice in page error.log
 #full = api_string + line1 + "&P=" + str("%.2f" %pressure) + "&W=0.0&G=0.0&B=0&R=0.0&RR=0.0&S=0.0" + "&TIN=" + str("%.2f" %cTemp) + "&HIN=" + str("%.2f" %hum) + "&BAT=91.2" + "&TBAT=92.8" + "&T2BAT=95.5" + "&T1=" + str("%.2f" %temp) + "&T2=" + str("%.2f" %read_temp()) + "&PASS=" + api_pass
-full = api_string + line1 + line2 + line3 + "&P=" + str("%.2f" %pressure) + "&W=0.0&G=0.0&B=0&R=0.0&RR=0.0&S=0.0" + "&TIN=" + str("%.2f" %tempds) + "&HIN=" + str("%.2f" %hum) + "&PASS=" + api_pass
-print full
-response = urllib2.urlopen(full)
+#full = api_string + line1 + line2 + line3 + "&P=" + str("%.2f" %pressure) + "&W=0.0&G=0.0&B=0&R=0.0&RR=0.0&S=0.0" + "&TIN=" + str("%.2f" %tempds) + "&HIN=" + str("%.2f" %hum) + "&PASS=" + api_pass
+sensrf = api_string + line1
+print sensor.RF.Ext1.txt
+
+response = urllib2.urlopen(sensrf)
 html = response.read()
 print html
