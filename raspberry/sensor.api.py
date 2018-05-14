4761d9f2a08f31ae01c4ea8f059e1d0275ef9211
petoz <peterzaletal@gmail.com>
2018-05-14 16:36:26 +0200

    i

--- raspberry/sensor.api.py
+++ raspberry/sensor.api.py
@@ -189,25 +189,25 @@ pressure = pressure * math.exp(gmhrtk)
 
 casi = int(time.time())
 cass = str(casi)
-api_string = "http://coffee.onmypc.net/meteotemplate/api.php?"
+api_string = "http://coffee.onmypc.net/meteotemplate/api.php?U="
 #full = api_string + line1 + "&TIN=" + str("%.2f" %cTemp) + "&P=" + str("%.2f" %pressure) + "&HIN=" + str("%.2f" %hum) + "&PASS=" + api_pass
 #full = api_string + line1 + "&P=" + str("%.2f" %pressure) + "&TIN=" + str("%.2f" %cTemp) + "&HIN=" + str("%.2f" %hum) + "&T1=" + str("%.2f" %temp) + "&PASS=" + api_pass
 #full = api_string + line1 + "&P=" + str("%.2f" %pressure) + "&TIN=" + str("%.2f" %cTemp) + "&HIN=" + str("%.2f" %hum) + "&T1=" + str("%.2f" %temp) + "&T2=" + str("%.2f" %read_temp()) + "&PASS=" + api_pass
 # 9.6.2017 add parameter W,G,B,R,RR,S to disable PHP Notice in page error.log
 #full = api_string + line1 + "&P=" + str("%.2f" %pressure) + "&W=0.0&G=0.0&B=0&R=0.0&RR=0.0&S=0.0" + "&TIN=" + str("%.2f" %cTemp) + "&HIN=" + str("%.2f" %hum) + "&BAT=91.2" + "&TBAT=92.8" + "&T2BAT=95.5" + "&T1=" + str("%.2f" %temp) + "&T2=" + str("%.2f" %read_temp()) + "&PASS=" + api_pass
-#sensorRFInt = api_string + "U=" + cass + "&P=" + str("%.2f" %pressure) + "&W=0.0&G=0.0&B=0&R=0.0&RR=0.0&S=0.0" + "&TIN=" + str("%.2f" %tempds) + "&HIN=" + str("%.2f" %hum) + "&PASS=" + api_pass
-sensorRFExt1 = api_string + line1 + "&PASS=" + api_pass
-sensorRFExt2 = api_string + line2 + "&PASS=" + api_pass
+sensorRFInt = api_string + cass + "&P=" + str("%.2f" %pressure) + "&W=0.0&G=0.0&B=0&R=0.0&RR=0.0&S=0.0" + "&TIN=" + str("%.2f" %tempds) + "&HIN=" + str("%.2f" %hum) + "&PASS=" + api_pass
+sensorRFExt1 = api_string + cass + line1 + "&PASS=" + api_pass
+sensorRFExt2 = api_string + cass + line2 + "&PASS=" + api_pass
 sensorRFExt3 = api_string + line3 + "&PASS=" + api_pass
 #full = api_string + line1 + line2 + line3 + "&P=" + str("%.2f" %pressure) + "&W=0.0&G=0.0&B=0&R=0.0&RR=0.0&S=0.0" + "&TIN=" + str("%.2f" %tempds) + "&HIN=" + str("%.2f" %hum) + "&PASS=" + api_pass
-#print sensorRFInt
+print sensorRFInt
 print sensorRFExt1
 print sensorRFExt2
 #print sensorRFExt3
 
-#response = urllib2.urlopen(sensorRFInt)
-#html = response.read()
-#print html
+response = urllib2.urlopen(sensorRFInt)
+html = response.read()
+print html
 
 response = urllib2.urlopen(sensorRFExt1)
 html = response.read()
