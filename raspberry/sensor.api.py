7e8281bd2af51e834a0857fd271aed2a4603aac1
petoz <peterzapletal@gmail.com>
2018-05-15 13:51:13 +0200

    clean code, alll sensors from external files

--- raspberry/sensor.api.py
+++ raspberry/sensor.api.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python
 
-import Adafruit_DHT
-import time
-import smbus
 import os
 import urllib2
-import math
 
 #password file for api
 api_pass_file = '/root/api_pass'
@@ -32,59 +28,50 @@ if os.path.isfile(infileInt):
         os.remove(infileInt)
 
 
-    #line1 = infile.readline()
-    #line1 = line1.rstrip()
+infileExt1 = '/var/log/mem/sensor.RF.Ext1.txt'
+if os.path.isfile(infileExt1):
+    sensorExt1 = open(infileExt1)
+    sensorExt1 = sensorExt1.readline()
+    sensorExt1 = sensorExt1.rstrip()
+    sensorExt1 = api_string + sensorExt1 + "&PASS=" + api_pass
+    print sensorExt1
+    response = urllib2.urlopen(sensorExt1)
+    html = response.read()
+    print html
+    if html == "Success":
+        html="success"
+        print html
+        print 'remove file'
+        os.remove(infileExt1)
 
-#print line1o
+infileExt2 = '/var/log/mem/sensor.RF.Ext2.txt'
+if os.path.isfile(infileExt2):
+    sensorExt2 = open(infileExt2)
+    sensorExt2 = sensorExt2.readline()
+    sensorExt2 = sensorExt2.rstrip()
+    sensorExt2 = api_string + sensorExt2 + "&PASS=" + api_pass
+    print sensorExt2
+    response = urllib2.urlopen(sensorExt2)
+    html = response.read()
+    print html
+    if html == "Success":
+        html="success"
+        print html
+        print 'remove file'
+        os.remove(infileExt2)
 
-
-infile = '/var/log/mem/sensor.RF.Ext1.txt'
-infile = open(infile)
-line1 = infile.readline()
-line1 = line1.rstrip()
-print line1
-
-infile2 = '/var/log/mem/sensor.RF.Ext2.txt'
-infile2 = open(infile2)
-line2 = infile2.readline()
-line2 = line2.rstrip()
-print line2
-
-infile3 = '/var/log/mem/sensor.RF.Ext3.txt'
-infile3 = open(infile3)
-line3 = infile3.readline()
-line3 = line3.rstrip()
-print line3
-
-casi = int(time.time())
-cass = str(casi)
-#full = api_string + line1 + "&TIN=" + str("%.2f" %cTemp) + "&P=" + str("%.2f" %pressure) + "&HIN=" + str("%.2f" %hum) + "&PASS=" + api_pass
-#full = api_string + line1 + "&P=" + str("%.2f" %pressure) + "&TIN=" + str("%.2f" %cTemp) + "&HIN=" + str("%.2f" %hum) + "&T1=" + str("%.2f" %temp) + "&PASS=" + api_pass
-#full = api_string + line1 + "&P=" + str("%.2f" %pressure) + "&TIN=" + str("%.2f" %cTemp) + "&HIN=" + str("%.2f" %hum) + "&T1=" + str("%.2f" %temp) + "&T2=" + str("%.2f" %read_temp()) + "&PASS=" + api_pass
-# 9.6.2017 add parameter W,G,B,R,RR,S to disable PHP Notice in page error.log
-#full = api_string + line1 + "&P=" + str("%.2f" %pressure) + "&W=0.0&G=0.0&B=0&R=0.0&RR=0.0&S=0.0" + "&TIN=" + str("%.2f" %cTemp) + "&HIN=" + str("%.2f" %hum) + "&BAT=91.2" + "&TBAT=92.8" + "&T2BAT=95.5" + "&T1=" + str("%.2f" %temp) + "&T2=" + str("%.2f" %read_temp()) + "&PASS=" + api_pass
-#sensorRFInt = api_string + cass + "&P=" + str("%.2f" %pressure) + "&W=0.0&G=0.0&B=0&R=0.0&RR=0.0&S=0.0" + "&TIN=" + str("%.2f" %tempds) + "&HIN=" + str("%.2f" %hum) + "&PASS=" + api_pass
-sensorRFExt1 = api_string + line1 + "&PASS=" + api_pass
-sensorRFExt2 = api_string + line2 + "&PASS=" + api_pass
-sensorRFExt3 = api_string + line3 + "&PASS=" + api_pass
-#full = api_string + line1 + line2 + line3 + "&P=" + str("%.2f" %pressure) + "&W=0.0&G=0.0&B=0&R=0.0&RR=0.0&S=0.0" + "&TIN=" + str("%.2f" %tempds) + "&HIN=" + str("%.2f" %hum) + "&PASS=" + api_pass
-#print sensorInt
-print sensorRFExt1
-print sensorRFExt2
-#print sensorRFExt3
-
-#response = urllib2.urlopen(sensorInt)
-#html = response.read()
-#print html
-
-response = urllib2.urlopen(sensorRFExt1)
-html = response.read()
-print html
-
-response = urllib2.urlopen(sensorRFExt2)
-html = response.read()
-print html
-
-#response = urllib2.urlopen(sensorRFExt3)
-#html = response.read()
-#print html
+infileExt3 = '/var/log/mem/sensor.RF.Ext3.txt'
+if os.path.isfile(infileExt3):
+    sensorExt3 = open(infileExt3)
+    sensorExt3 = sensorExt3.readline()
+    sensorExt3 = sensorExt3.rstrip()
+    sensorExt3 = api_string + sensorExt3 + "&PASS=" + api_pass
+    print sensorExt3
+    response = urllib2.urlopen(sensorExt3)
+    html = response.read()
+    print html
+    if html == "Success":
+        html="success"
+        print html
+        print 'remove file'
+        os.remove(infileExt3)
